8198fbd1d9f3c19c52d6f73fdc250dc17ab2d124
Amiruddin Irsyad <amiruddinid@gmail.com>
2025-12-09 17:58:21 +0700

    [WIP] employe crud

---
+++ src/main/java/com/crud/crud/Employee.java
@@ -0,0 +1,10 @@
+package com.crud.crud;
+
+// Model
+public class Employee {
+  public int id;
+  public String name;
+  public String email;
+  public String password;
+  public String noreg;
+}
---
+++ src/main/java/com/crud/crud/EmployeeController.java
@@ -0,0 +1,48 @@
+package com.crud.crud;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+//RESTful API
+// Controller
+@RestController
+public class EmployeeController {
+  //InMemory Database
+  // [key, value]
+  private List<Employee> employeeList = new ArrayList<>();
+  //READ
+  //http://localhost:8080/hello
+  @GetMapping("/hello") //EndpointRouter
+  public String index() {
+    return "Hello World Mantap";
+  }
+
+  //READ
+  @GetMapping("/employees")
+  public List<Employee> getEmployee(){
+    return employeeList;
+  }
+
+  //READ by ID
+  // Array ["data1", "data2"]
+  @GetMapping("/employees/{id}")
+  public Employee getEmployeeById(@PathVariable int id){
+    return employeeList.get(id);
+  }
+
+  //CREATE
+  @PostMapping("/employees")
+  public Employee createEmployee(@RequestBody Employee employee){
+    employeeList.add(employee);
+    return employee;
+  }
+
+  //UPDATE
+  //DELETE
+}
